8ecb88735986bc754d16cc95714ba84a7c26e6ae
강병헌 (kang byeonghun) <fund88@naver.com>
2023-08-24 00:41:56 +0900

    [Bronze III] Title: Not A + B, Time: 316 ms, Memory: 20444 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner ;
+
+public class Main{
+    public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    
+    int n= in.nextInt();
+    for(int i=0; i<n; i++){
+        String a = in.next();
+        String b = in.next();
+        System.out.println("1");
+        }
+    }
+}
