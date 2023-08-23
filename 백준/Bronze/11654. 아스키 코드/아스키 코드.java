ece8f313279f95e7d704d4f1f4754c05df020ca5
강병헌 (kang byeonghun) <fund88@naver.com>
2023-08-24 00:43:40 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        char word = in.next().charAt(0);
+        System.out.println((int) word);
+    }
+}
