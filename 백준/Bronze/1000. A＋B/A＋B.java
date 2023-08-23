8c74697d9691f42271dc06fc458d7ddf2cd12e51
강병헌 (kang byeonghun) <fund88@naver.com>
2023-08-23 12:17:53 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int str = sc.nextInt();
+    int str2= sc.nextInt();
+    System.out.println(str+str2);
+    }
+}
+
