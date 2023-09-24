27d62f1244e6472a4b73f99bc32132ab396af006
강병헌 (kang byeonghun) <fund88@naver.com>
2023-09-24 20:33:44 +0900

    [Bronze II] Title: Hashing, Time: 132 ms, Memory: 14196 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        String str = br.readLine();
-        int arr[] = new int[n];
-        long hashNum = 0;
-
-        for (int i = 0; i < n; i++) {
-           int a =str.charAt(i)-'a'+1;
-           hashNum += a * ((int)Math.pow(31,i));
-        }
-        System.out.println(hashNum);
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+		String S = br.readLine();
+		long result = 0;
+		long pow = 1;
+		for(int i = 0; i < L; i++) {
+			result += ((S.charAt(i) - 96) * pow);
+			//pow는 31을 매번 곱해준다. 곱해줄때마다 1234567891을 나눠주면 long을 넘지 않을 것이다.
+			pow = (pow * 31) % 1234567891;
+		}
+		System.out.println(result % 1234567891);
+	}
 
 }
