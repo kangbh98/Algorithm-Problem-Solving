8008c9f6b869e120add1ec237b3182a74f6fc0be
강병헌 (kang byeonghun) <fund88@naver.com>
2024-01-15 22:53:29 +0900

    [Silver I] Title: IOIOI, Time: 272 ms, Memory: 22036 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,58 +1,33 @@
-import java.util.*;
 import java.io.*;
-import java.lang.*;
 
+public class Main {
 
-public class Main{
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws Exception{
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		char s[] = br.readLine().toCharArray();
 
+		int result = 0;
+		int count = 0;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for(int i=1; i < M - 1; i++) {
+		if(s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+				count++;
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        char arr[] = new char[m];
-        char pNarr[] = new char[1+2*n];
+				if(count == N) {
+					count--;
+					result++;
+				}
+                i++;
+			}
+			else {
+				count = 0;
+			}
+		}
 
-        for (int i = 0; i < m; i++) {
-            arr[i] = s.charAt(i);
-        }
+		System.out.println(result);
 
-        pNarr[0] = 'I';
-        for (int i = 1; i < pNarr.length; i++) {
-
-            if (i % 2 == 1) {
-                pNarr[i] = 'O';
-            }else {
-                pNarr[i] = 'I';
-            }
-        }
-
-        int flag = 0;
-        int count = 0;
-        while (flag < m +1 - pNarr.length) {
-            char c = arr[flag];
-
-            if(c=='O'){
-                flag++;
-                continue;
-            }else {
-                boolean tf = true;
-                for (int i = 1; i < pNarr.length; i++) {
-                    if(arr[flag+i] != pNarr[i]){
-                           tf = false;
-                    }
-                }
-                if(tf == true){
-                    count++;
-                    flag+=2;
-                    continue;
-                }
-                flag++;
-            }
-        }
-        System.out.println(count);
-    }
-}
+	} // End of main
+} // End of class
